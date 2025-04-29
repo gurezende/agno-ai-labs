2f7f675832a64c033834535cc93c61fdf6d38c77
Gustavo Santos <gurezendes@gmail.com>
2025-04-29 13:30:41 -0400

    Upd: inserted comments

--- scripts/file_handling.py
+++ scripts/file_handling.py
@@ -16,9 +16,10 @@ agent = Agent(
                   show_tool_calls=True)
 
 
-
+# Writing and saving a file
 agent.print_response("Write a 300 word summary about price optimization with Python and save it to a file named 'price_optimization.md'.",
                      markdown=True)
 
+# Reading a file
 agent.print_response("Read the file 'pyproject.toml' and tell me the content in the 'dependencies' section.",
                      markdown=True)
